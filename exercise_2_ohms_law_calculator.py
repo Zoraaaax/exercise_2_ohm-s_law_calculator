088653e9d1820c56f58c1a59b33f8e2a87800e03
Kimberly Aguilar <ae.xerx@gmail.com>
2024-09-25 15:08:12 +0800

    calculate_voltage_using_ohm's_law

--- exercise_2_ohms_law_calculator.py
+++ exercise_2_ohms_law_calculator.py
@@ -1,2 +1,7 @@
 calculate = str(input("What do you want to calculate: Voltage, Current, or Resistance? ")).lower()
 
+if calculate == "voltage":
+    current = float(input("Enter the value of Current (in amperes): "))
+    resistance = float(input("Enter the value of Resistance (in ohms): "))
+    total_voltage = current * resistance
+    print(f"The Voltage is {total_voltage} volts")
